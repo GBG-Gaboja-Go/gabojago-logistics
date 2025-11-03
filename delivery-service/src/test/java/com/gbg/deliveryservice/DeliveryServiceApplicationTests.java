04b5ce7f3ab6c6a043e1e6eab5af7050028e83a9
changhui98 <net_0102@naver.com>
2025-11-03 22:34:24 +0900

    Feat : delivery-service init commit

---
+++ delivery-service/src/test/java/com/gbg/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbg.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
