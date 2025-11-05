57bf1f2d9b93968648921668cc75c2d9ee77539a
wnsgud0310 <ffjjo0310@naver.com>
2025-11-05 20:22:23 +0900

    feat: 허브 생성 dto 구현

---
+++ hub-service/src/main/java/com/gbg/hubservice/presentation/dto/request/PostHubRequestDto.java
@@ -0,0 +1,40 @@
+package com.gbg.hubservice.presentation.dto.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostHubRequestDto {
+
+    @NotNull(message = "허브 정보를 입력해주세요.")
+    @Valid
+    private HubDto hub;
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class HubDto {
+
+        @NotBlank(message = "허브 이름을 입력해주세요.")
+        private String name;
+
+        @NotBlank(message = "주소를 입력해주세요.")
+        private String address;
+
+        @NotNull(message = "위도를 입력해주세요.")
+        private BigDecimal latitude;
+        
+        @NotNull(message = "경도를 입력해주세요.")
+        private BigDecimal longitude;
+    }
+}
