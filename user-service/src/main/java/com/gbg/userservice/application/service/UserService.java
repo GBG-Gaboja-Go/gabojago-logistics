46921fcdef39bfe299172900febe797fd20dbb22
changhui98 <net_0102@naver.com>
2025-11-06 21:32:56 +0900

    Feat : 관리자 전용 사용자 정보 수정 기능 구현

--- user-service/src/main/java/com/gbg/userservice/application/service/UserService.java
+++ user-service/src/main/java/com/gbg/userservice/application/service/UserService.java
@@ -1,6 +1,7 @@
 package com.gbg.userservice.application.service;
 
 import com.gbg.userservice.infrastructure.config.auth.CustomUser;
+import com.gbg.userservice.presentation.dto.request.AdminUpdateRequestDto;
 import com.gbg.userservice.presentation.dto.request.UserUpdateRequestDto;
 import com.gbg.userservice.presentation.dto.response.UserListResponseDto;
 import com.gbg.userservice.presentation.dto.response.UserResponseDto;
@@ -16,4 +17,6 @@ public interface UserService {
     UUID userDetailUpdate(UserUpdateRequestDto req, UUID id);
 
     void userDelete(UUID loginId, UUID userId);
+
+    UUID adminDetailUpdate(UUID loginId, UUID userId, AdminUpdateRequestDto req);
 }
