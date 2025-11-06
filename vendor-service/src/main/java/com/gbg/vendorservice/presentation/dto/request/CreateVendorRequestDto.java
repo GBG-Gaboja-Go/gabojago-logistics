133107be50200739d49e15e093a70e64d0ed403d
BackToTheDev <rnjswnstjd13@gmail.com>
2025-11-06 20:33:12 +0900

    Feat: Vendor controller, dto 개발

---
+++ vendor-service/src/main/java/com/gbg/vendorservice/presentation/dto/request/CreateVendorRequestDto.java
@@ -0,0 +1,30 @@
+package com.gbg.vendorservice.presentation.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateVendorRequestDto {
+
+    @NotNull(message = "업체명을 입력해주세요.")
+    private String name;
+
+    @NotNull(message = "허브 ID를 입력해주세요.")
+    private UUID hubId;
+
+    @NotNull(message = "매니저 ID를 입력해주세요.")
+    private UUID managerId;
+
+    @NotNull(message = "주소를 입력해주세요.")
+    private String address;
+
+    private boolean isSupplier;
+    private boolean isReceiver;
+}
