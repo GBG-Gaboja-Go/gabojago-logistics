1c8e30db6b2d191ccbc0f46c8a78e6a7a9f68015
gahyeon <1000ji1@naver.com>
2025-11-06 11:47:03 +0900

    Feat: baseResponseDto 적용, swagger 기능 설명 추가

--- order-service/src/main/java/com/gbg/orderservice/presentation/controller/OrderController.java
+++ order-service/src/main/java/com/gbg/orderservice/presentation/controller/OrderController.java
@@ -8,6 +8,8 @@ import com.gbg.orderservice.presentation.dto.request.OrderSearchRequestDto;
 import com.gbg.orderservice.presentation.dto.request.UpdateOrderStatusRequestDto;
 import com.gbg.orderservice.presentation.dto.response.CreateOrderResponseDto;
 import com.gbg.orderservice.presentation.dto.response.GetOrderResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import java.math.BigInteger;
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.stream.IntStream;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @PostMapping
-    // @Operation(summary = "주문 생성 API", description = "수령업체만 주문을 생성할 수 있습니다.")
+    @Operation(summary = "주문 생성 API", description = "수령업체만 주문을 생성할 수 있습니다.")
     public ResponseEntity<BaseResponseDto<CreateOrderResponseDto>> createOrder(
         @Valid @RequestBody CreateOrderRequestDto requestDto
     ) {
@@ -45,11 +48,12 @@ public class OrderController {
             )
             .build();
 
-        return ResponseEntity.ok(BaseResponseDto.success("주문 생성 성공", responseDto));
+        return ResponseEntity.ok(
+            BaseResponseDto.success("주문 생성 성공", responseDto, HttpStatus.CREATED));
     }
 
     @PostMapping("/search")
-    // @Operation(summary = "주문 전체 조회 API", description = "수령업체는 본인 주문을 모두 조회할 수 있습니다.(마스터는 모든 수령업체 주문 조회 가능)")
+    @Operation(summary = "주문 전체 조회 API", description = "수령업체는 본인 주문을 모두 조회할 수 있습니다.(마스터는 모든 수령업체 주문 조회 가능)")
     public ResponseEntity<BaseResponseDto<PageResponseDto<GetOrderResponseDto>>> getOrders(
         OrderSearchRequestDto searchRequestDto,
         Pageable pageable
@@ -77,13 +81,13 @@ public class OrderController {
         );
 
         return ResponseEntity.ok(
-            BaseResponseDto.success("메뉴 목록 조회 성공", PageResponseDto.from(result)));
+            BaseResponseDto.success("메뉴 목록 조회 성공", PageResponseDto.from(result), HttpStatus.OK));
     }
 
     @GetMapping("/{orderId}")
-    // @Operation(summary = "상세 주문 조회 API", description = "상세 주문을 조회합니다.")
+    @Operation(summary = "상세 주문 조회 API", description = "상세 주문을 조회합니다.")
     public ResponseEntity<BaseResponseDto<GetOrderResponseDto>> getOrder(
-        @PathVariable UUID orderId
+        @Parameter(description = "order UUID") @PathVariable UUID orderId
     ) {
         GetOrderResponseDto responseDto = GetOrderResponseDto.builder()
             .order(
@@ -100,25 +104,26 @@ public class OrderController {
                     .build()
             )
             .build();
-        return ResponseEntity.ok(BaseResponseDto.success("상세 주문 조회 성공", responseDto));
+        return ResponseEntity.ok(
+            BaseResponseDto.success("상세 주문 조회 성공", responseDto, HttpStatus.OK));
     }
 
     @PatchMapping("/{orderId}")
-    // @Operation(summary = "주문 상태 업데이트 API", description = "마스터와 담당 허브 관리자는 상세 주문을 업데이트할 수 있습니다.")
+    @Operation(summary = "주문 상태 업데이트 API", description = "마스터와 담당 허브 관리자는 상세 주문을 업데이트할 수 있습니다.")
     public ResponseEntity<BaseResponseDto<Void>> updateOrderStatus(
         @Valid @RequestBody UpdateOrderStatusRequestDto requestDto,
-        @PathVariable UUID orderId
+        @Parameter(description = "order UUID") @PathVariable UUID orderId
     ) {
         // order status 변경
-        return ResponseEntity.ok(BaseResponseDto.success("주문 상태 변경 성공"));
+        return ResponseEntity.ok(BaseResponseDto.success("주문 상태 변경 성공", HttpStatus.NO_CONTENT));
     }
 
     @DeleteMapping("/{orderId}")
-    // @Operation(summary = "주문 취소 API", description = "상품 담당 허브 관리자와 공급업체는 주문을 취소할 수 있습니다.")
+    @Operation(summary = "주문 취소 API", description = "상품 담당 허브 관리자와 공급업체는 주문을 취소할 수 있습니다.")
     public ResponseEntity<BaseResponseDto<Void>> cancelOrder(
-        @PathVariable UUID orderId
+        @Parameter(description = "order UUID") @PathVariable UUID orderId
     ) {
         // order 취소
-        return ResponseEntity.ok(BaseResponseDto.success("주문 취소 성공"));
+        return ResponseEntity.ok(BaseResponseDto.success("주문 취소 성공", HttpStatus.NO_CONTENT));
     }
 }
