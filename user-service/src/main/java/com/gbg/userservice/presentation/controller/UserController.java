064bb193d64e7977f465ccf0251937b0e0885562
changhui98 <net_0102@naver.com>
2025-11-06 21:32:56 +0900

    Feat : 관리자 전용 사용자 정보 수정 기능 구현

--- user-service/src/main/java/com/gbg/userservice/presentation/controller/UserController.java
+++ user-service/src/main/java/com/gbg/userservice/presentation/controller/UserController.java
@@ -4,6 +4,7 @@ import com.gabojago.dto.BaseResponseDto;
 import com.gbg.userservice.application.service.AuthService;
 import com.gbg.userservice.application.service.UserService;
 import com.gbg.userservice.infrastructure.config.auth.CustomUser;
+import com.gbg.userservice.presentation.dto.request.AdminUpdateRequestDto;
 import com.gbg.userservice.presentation.dto.request.CreateUserRequestDto;
 import com.gbg.userservice.presentation.dto.request.UserUpdateRequestDto;
 import com.gbg.userservice.presentation.dto.response.UserListResponseDto;
@@ -48,6 +49,7 @@ public class UserController {
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('MASTER')")
     public ResponseEntity<BaseResponseDto<List<UserListResponseDto>>> userList() {
 
         List<UserListResponseDto> userList = userService.getUserList();
@@ -85,6 +87,24 @@ public class UserController {
             .status(HttpStatus.OK)
             .body(BaseResponseDto.success(
                 "사용자 정보가 수정되었습니다.",
+                user,
+                HttpStatus.OK
+            ));
+    }
+
+    @PatchMapping("/my-page/{userId}")
+    @PreAuthorize("hasRole('MASTER')")
+    public ResponseEntity<BaseResponseDto<UUID>> adminDetailUpdate(
+        @AuthenticationPrincipal CustomUser customUser,
+        @RequestBody AdminUpdateRequestDto req,
+        @PathVariable("userId") UUID userId
+    ) {
+        UUID user = userService.adminDetailUpdate(customUser.getId(), userId, req);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(BaseResponseDto.success(
+                "사용자 정보가 정상적으로 수정되었습니다.",
                 user,
                 HttpStatus.OK
             ));
