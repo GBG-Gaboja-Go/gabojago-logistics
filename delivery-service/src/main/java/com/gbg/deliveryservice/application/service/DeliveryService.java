edd12edf94f47b54f958384d75d44cc50d468c29
taeaeaeae <haenimhana@gmail.com>
2025-11-07 15:06:15 +0900

    Feat : 비지니스 로직 작성 전 틀 구성

--- delivery-service/src/main/java/com/gbg/deliveryservice/application/service/DeliveryService.java
+++ delivery-service/src/main/java/com/gbg/deliveryservice/application/service/DeliveryService.java
@@ -1,5 +1,32 @@
 package com.gbg.deliveryservice.application.service;
 
-public class DeliveryService {
+import com.gbg.deliveryservice.domain.entity.enums.DeliveryStatus;
+import com.gbg.deliveryservice.presentation.dto.request.CreateDeliveryRequestDTO;
+import com.gbg.deliveryservice.presentation.dto.request.UpdateDeliveryRequestDTO;
+import com.gbg.deliveryservice.presentation.dto.request.UpdateDeliveryStatusRequestDTO;
+import com.gbg.deliveryservice.presentation.dto.response.CreateDeliveryResponseDTO;
+import com.gbg.deliveryservice.presentation.dto.response.GetDeliveryPageResponseDTO;
+import com.gbg.deliveryservice.presentation.dto.response.GetDeliveryResponseDTO;
+import com.gbg.deliveryservice.presentation.dto.response.GetMyDeliveryResponseDTO;
+import java.util.UUID;
+import org.springframework.data.domain.Pageable;
 
+public interface DeliveryService {
+
+    CreateDeliveryResponseDTO createDelivery(CreateDeliveryRequestDTO req);
+
+    GetDeliveryPageResponseDTO getDeliveryPage(Pageable pageable, DeliveryStatus status,
+        UUID sender, UUID receiver, UUID product);
+
+    GetDeliveryResponseDTO getDelivery(UUID id);
+
+    Void updateDelivery(UpdateDeliveryRequestDTO req, UUID id, UUID userId);
+
+    Void updateDeliveryStatus(UpdateDeliveryStatusRequestDTO req, UUID id, UUID userId);
+
+    GetMyDeliveryResponseDTO getMyDeliveryPage(UUID userId, Pageable pageable,
+        DeliveryStatus status,
+        UUID sender, UUID receiver, UUID product);
+
+    Void deleteDelivery(UUID id);
 }
---
+++ delivery-service/src/main/java/com/gbg/deliveryservice/domain/repository/HubDeliveryRepository.java
@@ -0,0 +1,5 @@
+package com.gbg.deliveryservice.domain.repository;
+
+public interface HubDeliveryRepository {
+
+}
