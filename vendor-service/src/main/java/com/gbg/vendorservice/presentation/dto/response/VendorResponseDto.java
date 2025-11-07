9a9e65cf7b18b32c87c0c038d2d4ecc4c0dc0cad
BackToTheDev <rnjswnstjd13@gmail.com>
2025-11-07 13:51:52 +0900

    "Refactor: VendorResponseDto 피드백 반영"

--- vendor-service/src/main/java/com/gbg/vendorservice/presentation/dto/response/VendorResponseDto.java
+++ vendor-service/src/main/java/com/gbg/vendorservice/presentation/dto/response/VendorResponseDto.java
@@ -1,6 +1,5 @@
 package com.gbg.vendorservice.presentation.dto.response;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,13 +19,4 @@ public class VendorResponseDto {
     private boolean supplier;
     private boolean receiver;
     private String address;
-
-    private String createdBy;
-    private LocalDateTime createdAt;
-    private String updatedBy;
-    private LocalDateTime updatedAt;
-    private String deletedBy;
-    private LocalDateTime deletedAt;
-    private boolean deleted;
-
 }
