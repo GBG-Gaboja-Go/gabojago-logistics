a261c0b54401f19ddd430267ab4edbd6a1f18da1
changhui98 <net_0102@naver.com>
2025-11-07 14:17:33 +0900

    Feat : 게이트웨이 Global 필터에서 jwt검증 기능 추가

---
+++ gateway-service/src/main/java/com/gbg/gatewayservice/config/jwt/JwtTokenProvider.java
@@ -0,0 +1,38 @@
+package com.gbg.gatewayservice.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public boolean validateToken(String token) {
+
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+}
