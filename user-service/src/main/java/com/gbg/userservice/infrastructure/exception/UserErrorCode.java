c04246948e86c41083175c852a4b1dcc4c8b23ca
changhui98 <net_0102@naver.com>
2025-11-07 10:49:57 +0900

    Feat : deletedAt, By 필드값이 있는 사용자 로그인 제한 기능 추가

--- user-service/src/main/java/com/gbg/userservice/infrastructure/exception/UserErrorCode.java
+++ user-service/src/main/java/com/gbg/userservice/infrastructure/exception/UserErrorCode.java
@@ -9,10 +9,11 @@ public enum UserErrorCode implements ErrorCode {
 
     USER_UNAUTHORIZED("USER000", "사용자 인증에 실패하였습니다.", HttpStatus.UNAUTHORIZED),
     USER_NOT_FOUND("USER001", "해당 사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    USER_DUPLICATE_NAME("USER002", "이미 사용중인 ID 입니다.", HttpStatus.CONFLICT),
-    USER_DUPLICATE_EMAIL("USER003", "이미 사용중인 Email 입니다.", HttpStatus.CONFLICT),
+    USER_DUPLICATE_NAME("USER002", "이미 사용중인 ID 입니다.", HttpStatus.BAD_REQUEST),
+    USER_DUPLICATE_EMAIL("USER003", "이미 사용중인 Email 입니다.", HttpStatus.BAD_REQUEST),
     USER_FORBIDDEN("USER004", "사용자 권한이 없습니다.", HttpStatus.FORBIDDEN),
-    SELF_ROLE_UPDATE("USER005", "사용자 권한은 직접 변경할 수 없습니다.", HttpStatus.CONFLICT)
+    SELF_ROLE_UPDATE("USER005", "사용자 권한은 직접 변경할 수 없습니다.", HttpStatus.FORBIDDEN),
+    DELETED_USER("USER006", "탈퇴된 사용자 입니다.", HttpStatus.NOT_FOUND),
     ;
 
     private final String code;
