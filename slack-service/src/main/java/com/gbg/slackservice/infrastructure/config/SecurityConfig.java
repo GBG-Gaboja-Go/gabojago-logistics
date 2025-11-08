4cbff052a7d568c01b4ba1108be9a7b4cc82e545
changhui98 <net_0102@naver.com>
2025-11-08 18:09:09 +0900

    Feat : Slack 서비스 시큐리티 설정

---
+++ slack-service/src/main/java/com/gbg/slackservice/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.gbg.slackservice.infrastructure.config;
+
+import com.gbg.slackservice.infrastructure.config.filter.HeaderAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final HeaderAuthFilter headerAuthFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+        DefaultAuthenticationEventPublisher authenticationEventPublisher) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/v1/slacks/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+
+}
