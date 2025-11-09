39948cbba62ed2382e6ac29423747761e1dbe53a
wnsgud0310 <ffjjo0310@naver.com>
2025-11-09 15:22:42 +0900

    refactor: 공통 모듈 예외처리 사용

--- hub-service/src/main/java/com/gbg/hubservice/application/service/exception/HubErrorCode.java
+++ hub-service/src/main/java/com/gbg/hubservice/application/service/exception/HubErrorCode.java
@@ -1,27 +1,19 @@
 package com.gbg.hubservice.application.service.exception;
 
+import com.gabojago.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-public enum HubErrorCode {
+@Getter
+@RequiredArgsConstructor
+public enum HubErrorCode implements ErrorCode {
 
-    HUB_NOT_FOUND(HttpStatus.NOT_FOUND, "허브를 찾을 수 없습니다."),
-    HUB_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 허브 입니다."),
-    INVALID_HUB_REQUEST(HttpStatus.BAD_REQUEST, "허브 요청 정보가 올바르지 않습니다."),
-    HUB_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "허브 삭제 처리 중 오류가 발생했습니다.");
+    HUB_NOT_FOUND(HttpStatus.NOT_FOUND, "허브를 찾을 수 없습니다.", "HUB_404"),
+    HUB_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 허브입니다.", "HUB_409"),
+    HUB_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "허브 접근 권한이 없습니다.", "HUB_403");
 
     private final HttpStatus status;
     private final String message;
-
-    HubErrorCode(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    private final String code;
 }
