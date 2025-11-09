41680b74b2fe52297f328577d111c2a4d25d5b18
gahyeon <1000ji1@naver.com>
2025-11-09 17:38:31 +0900

    Refactor: product dto 구조 변경

--- product-service/src/main/java/com/gbg/productservice/presentation/dto/request/UpdateProductRequestDto.java
+++ product-service/src/main/java/com/gbg/productservice/presentation/dto/request/UpdateProductRequestDto.java
@@ -1,5 +1,8 @@
 package com.gbg.productservice.presentation.dto.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigInteger;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,8 +15,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UpdateProductRequestDto {
 
-    private String name;
-    private UUID vendorId;
-    private Integer stock;
-    private Integer price;
+    @Valid
+    @NotNull(message = "상품 정보를 입력해주세요.")
+    private ProductDto product;
+
+    @Getter
+    @Builder
+    public static class ProductDto {
+
+        private String name;
+        private UUID vendorId;
+        private Integer stock;
+        private BigInteger price;
+
+    }
+
 }
--- product-service/src/main/java/com/gbg/productservice/presentation/dto/response/ProductResponseDto.java
+++ product-service/src/main/java/com/gbg/productservice/presentation/dto/response/ProductResponseDto.java
@@ -1,5 +1,6 @@
 package com.gbg.productservice.presentation.dto.response;
 
+import java.math.BigInteger;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,9 +13,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProductResponseDto {
 
-    private UUID id;
-    private String name;
-    private UUID vendorId;
-    private Integer stock;
-    private Integer price;
+    private ProductDto product;
+
+
+    @Getter
+    @Builder
+    public static class ProductDto {
+
+        private final UUID id;
+        private final String name;
+        private final UUID vendorId;
+        private final Integer stock;
+        private final BigInteger price;
+    }
 }
