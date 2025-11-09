a201d23e2324b80a0c9934a2e90f77ddeaf8d248
gahyeon <1000ji1@naver.com>
2025-11-09 18:58:52 +0900

    Feat: restTemplate으로 product 조회

---
+++ order-service/src/main/java/com/gbg/orderservice/infrastructure/resttemplate/product/dto/response/ProductResponseDto.java
@@ -0,0 +1,29 @@
+package com.gbg.orderservice.infrastructure.resttemplate.product.dto.response;
+
+import java.math.BigInteger;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponseDto {
+
+    private ProductDto product;
+
+
+    @Getter
+    @Builder
+    public static class ProductDto {
+
+        private final UUID id;
+        private final String name;
+        private final UUID vendorId;
+        private final Integer stock;
+        private final BigInteger price;
+    }
+}
