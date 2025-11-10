9cb109dd55bc61dcf71c7ae692a9b550a52f22b0
wnsgud0310 <ffjjo0310@naver.com>
2025-11-10 21:26:16 +0900

    feat: 게이트웨이 기반 인증 구현

---
+++ hub-service/src/main/java/com/gbg/hubservice/infrastructure/config/auth/CustomUser.java
@@ -0,0 +1,52 @@
+package com.gbg.hubservice.infrastructure.config.auth;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUser implements UserDetails {
+
+    private final UUID userId;
+    private final List<? extends GrantedAuthority> authorities;
+
+    @Override
+    public String getUsername() {
+        return userId.toString();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+}
