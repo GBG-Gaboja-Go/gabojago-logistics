80ceb06364b2421ce9eac7733ce9107edd5e93c0
wnsgud0310 <ffjjo0310@naver.com>
2025-11-11 17:14:47 +0900

    feat: 허브 경로 관련 오류 코드 추가

--- hub-service/src/main/java/com/gbg/hubservice/application/service/exception/HubErrorCode.java
+++ hub-service/src/main/java/com/gbg/hubservice/application/service/exception/HubErrorCode.java
@@ -11,7 +11,10 @@ public enum HubErrorCode implements ErrorCode {
 
     HUB_NOT_FOUND(HttpStatus.NOT_FOUND, "허브를 찾을 수 없습니다.", "HUB_404"),
     HUB_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 허브입니다.", "HUB_409"),
-    HUB_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "허브 접근 권한이 없습니다.", "HUB_403");
+    HUB_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "허브 접근 권한이 없습니다.", "HUB_403"),
+    HUB_ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "경로를 찾을 수 없습니다.", "ROUTE_404"),
+    HUB_ROUTE_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 경로입니다.", "ROUTE_409");
+
 
     private final HttpStatus status;
     private final String message;
