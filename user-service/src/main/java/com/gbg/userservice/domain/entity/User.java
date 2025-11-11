9946c4ea30859f60c952bcfbbcfdf717f58187b2
changhui98 <net_0102@naver.com>
2025-11-11 11:59:12 +0900

    Fix : 중복된 사용자 이름 회원가입 성공되는 버그 해결

--- user-service/src/main/java/com/gbg/userservice/domain/entity/User.java
+++ user-service/src/main/java/com/gbg/userservice/domain/entity/User.java
@@ -24,6 +24,7 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(unique = true)
     private String username;
 
     private String nickname;
