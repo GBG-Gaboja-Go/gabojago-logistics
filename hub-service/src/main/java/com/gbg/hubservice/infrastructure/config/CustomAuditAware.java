01cc114b15ce36098d19a0ca3d53a6a2ea4683da
wnsgud0310 <ffjjo0310@naver.com>
2025-11-11 11:21:33 +0900

    feat: Auditing 처리용 CustomAuditAware 추가

---
+++ hub-service/src/main/java/com/gbg/hubservice/infrastructure/config/CustomAuditAware.java
@@ -0,0 +1,38 @@
+package com.gbg.hubservice.infrastructure.config;
+
+import com.gbg.hubservice.infrastructure.config.auth.CustomUser;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class CustomAuditAware implements AuditorAware<UUID> {
+
+    @Override
+    public Optional<UUID> getCurrentAuditor() {
+
+        Authentication authentication
+            = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("[AuditorAware] : authentication : " + authentication);
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            System.out.println("[AuditorAware] 인증 정보가 없거나 인증되지 않은 상태입니다.");
+            return Optional.empty();
+        }
+
+        Object principal = authentication.getPrincipal();
+        System.out.println("[AuditorAware] : principal : " + principal);
+
+        if (principal instanceof CustomUser user) {
+            System.out.println("[AuditorAware] 로그인 사용자 ID : " + user.getId());
+            return Optional.ofNullable(user.getId());
+        }
+
+        System.out.println(
+            "[AuditorAware] Principal이 CustomUser 타입이 아닙니다. 현재 타입 : " + (principal != null
+                ? principal.getClass().getName() : "null"));
+        return Optional.empty();
+    }
+
+}
