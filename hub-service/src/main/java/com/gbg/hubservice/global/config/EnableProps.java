e76065bb020cd4c4ce8feb7a1cfc4deeb24618e9
wnsgud0310 <ffjjo0310@naver.com>
2025-11-12 01:36:01 +0900

    chore: application.yml 환경 변수 및 kakao 설정 추가

--- hub-service/src/main/java/com/gbg/hubservice/global/config/EnableProps.java
+++ hub-service/src/main/java/com/gbg/hubservice/global/config/EnableProps.java
@@ -1,13 +1,15 @@
+// src/main/java/com/gbg/hubservice/global/config/EnableProps.java
 package com.gbg.hubservice.global.config;
 
-import com.gbg.hubservice.infrastructure.kakao.KakaoApiProperties;
+import com.gbg.hubservice.infrastructure.config.kakao.*;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 @EnableConfigurationProperties({
-    KakaoApiProperties.class
+    KakaoApiProperties.class,
+    KakaoLocalProperties.class,
+    KakaoNaviProperties.class
 })
 public class EnableProps {
 
