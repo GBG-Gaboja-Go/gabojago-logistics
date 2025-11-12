73d343404bc93bb147304c2f0a1a01ef9cf3172d
taeaeaeae <haenimhana@gmail.com>
2025-11-12 14:56:49 +0900

    Fix : 에러 메세지 파싱

--- delivery-service/src/main/java/com/gbg/deliveryservice/presentation/advice/FeignClientError.java
+++ delivery-service/src/main/java/com/gbg/deliveryservice/presentation/advice/FeignClientError.java
@@ -1,6 +1,8 @@
 package com.gbg.deliveryservice.presentation.advice;
 
 import com.gabojago.exception.ErrorCode;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -13,8 +15,21 @@ public class FeignClientError implements ErrorCode {
     private String message;
     private HttpStatus status;
 
-    public static FeignClientError of(String code, String message, HttpStatus status) {
+    public static FeignClientError of(String code, String messages, HttpStatus status) {
+
+        String message = extractValue(messages, "\"message\"\\s*:\\s*\"([^\"]+)\"");
+
         return new FeignClientError(code, message, status);
+    }
+
+    private static String extractValue(String messages, String s) {
+
+        Pattern pattern = Pattern.compile(s);
+        Matcher matcher = pattern.matcher(messages);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
     }
 
 }
