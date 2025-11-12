9ff7ca35aa84d84a68f8fb8971cce528f32c5805
taeaeaeae <haenimhana@gmail.com>
2025-11-12 13:09:36 +0900

    Fix : header 값 변경

--- delivery-service/src/main/java/com/gbg/deliveryservice/infrastructure/client/OrderFeignClient.java
+++ delivery-service/src/main/java/com/gbg/deliveryservice/infrastructure/client/OrderFeignClient.java
@@ -4,11 +4,16 @@ import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "order-service")
 public interface OrderFeignClient {
 
     @GetMapping("/v1/orders/{orderId}")
-    void getOrderDetail(@PathVariable("orderId") UUID orderId);
+    void getOrderDetail(
+        @PathVariable("orderId") UUID orderId,
+        @RequestHeader("X-Auth-Id") String id,
+        @RequestHeader("X-Auth-Role") String role
+    );
 
 }
